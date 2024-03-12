1c6df304fd48253121e950d919a34523931d1ef8
EC2 Default User <ec2-user@ip-172-31-41-171.eu-north-1.compute.internal>
2024-03-12 13:53:04 +0000

    Creating a test to verify text is displayed in the app header

--- passing-thoughts/src/Thought.test.js
+++ passing-thoughts/src/Thought.test.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Thought } from "./Thought.js";
+import { App } from "./App.js";
+import '@testing-library/jest-dom';
 
 // Import render and screen here
 import { render, screen } from '@testing-library/react';
@@ -11,4 +13,11 @@ it("Displays the Thought component", () => {
   render(<Thought thought={thought} removeThought={() => {}} />);
   screen.debug();
   
+});
+
+it("Should have header text Passing Thoughts", () => {
+  render(<App />);
+  // Test App header text here
+  const header = screen.getByText('Passing Thoughts');
+  expect(header).toHaveTextContent('Passing Thoughts');
 });
